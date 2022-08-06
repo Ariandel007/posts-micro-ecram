e88d9d00d9ae2251b748ba5fabe6ffe99b16277d
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-08-06 00:55:01 -0500

    add secrets

--- src/main/java/com/ecram/usersmicroecram/posts/config/GeneralConfiguration.java
+++ src/main/java/com/ecram/usersmicroecram/posts/config/GeneralConfiguration.java
@@ -2,6 +2,7 @@ package com.ecram.usersmicroecram.posts.config;
 
 import com.cloudinary.Cloudinary;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +11,24 @@ import java.util.Map;
 
 @Configuration
 public class GeneralConfiguration {
+
+    @Value("${cloudinaryParams.cloudName}")
+    private String cloudName;
+
+    @Value("${cloudinaryParams.apiKey}")
+    private String apiKey;
+
+    @Value("${cloudinaryParams.apiSecret}")
+    private String apiSecret;
+
+
     @Bean(name = "cloudinaryConfig")
     public Cloudinary cloudinaryConfig() {
         Cloudinary cloudinary = null;
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "");
-        config.put("api_key", "");
-        config.put("api_secret", "");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         cloudinary = new Cloudinary(config);
         return cloudinary;
     }
