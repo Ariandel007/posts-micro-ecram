8fd21d01b1f55ec080412ff0a4a17db25073ba72
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-08-09 23:48:51 -0500

    add unit test schema

--- src/test/java/com/ecram/usersmicroecram/posts/services/GroupOperationsServiceTests.java
+++ src/test/java/com/ecram/usersmicroecram/posts/services/GroupOperationsServiceTests.java
@@ -1,10 +1,38 @@
 package com.ecram.usersmicroecram.posts.services;
 
+import com.ecram.usersmicroecram.posts.client.IUserClientRest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class GroupOperationsServiceTests {
 
+    @MockBean
+    ICloudinaryService cloudinaryService;
+
+    @MockBean
+    IGroupService groupService;
+
+    @MockBean
+    IUserClientRest userClientRest;
+
+    @MockBean
+    IFollowedGroupService followedGroupService;
+
+    @MockBean
+    ModelMapper modelMapper;
+
+    @Autowired
+    IGroupOperationsService groupOperationsService;
 
 
+    @Test
+    @DisplayName("Test - Registro de un Grupo (Group Operations)")
+    void testSaveFollowedGroup() {
+
+    }
 }
