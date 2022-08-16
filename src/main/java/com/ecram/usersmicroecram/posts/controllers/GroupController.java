b0e23af71e430712e90ae8e6b021906eb6e3a370
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-08-16 00:32:59 -0500

    Add controller

---
+++ src/main/java/com/ecram/usersmicroecram/posts/controllers/GroupController.java
@@ -0,0 +1,31 @@
+package com.ecram.usersmicroecram.posts.controllers;
+
+import com.ecram.usersmicroecram.posts.dtos.request.GroupToCreateDto;
+import com.ecram.usersmicroecram.posts.models.Group;
+import com.ecram.usersmicroecram.posts.services.IGroupOperationsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("v1/groups")
+public class GroupController {
+    private IGroupOperationsService groupOperationsService;
+
+    @Autowired
+    public GroupController(IGroupOperationsService groupOperationsService) {
+        this.groupOperationsService = groupOperationsService;
+    }
+
+    @PostMapping(value = "/createGroup/{username}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<Group> createGroup(@RequestPart GroupToCreateDto groupToCreateDto,
+                                      @RequestPart(value = "fileLogo", required = false) MultipartFile fileLogo,
+                                      @RequestPart(value = "fileBanner", required = false) MultipartFile fileBanner,
+                                      @PathVariable("username") String username) {
+        Group groupCreated = this.groupOperationsService.createGroup(username, fileLogo, fileBanner, groupToCreateDto);
+        return ResponseEntity.ok().body(groupCreated);
+    }
+
+}
