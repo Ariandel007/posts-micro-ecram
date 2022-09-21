0977792818a1159fad5f6030ccc41b5706a77d69
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2022-09-20 23:46:51 -0500

    add query for get a post

--- src/main/java/com/ecram/usersmicroecram/posts/repositories/IPostRepository.java
+++ src/main/java/com/ecram/usersmicroecram/posts/repositories/IPostRepository.java
@@ -2,9 +2,17 @@ package com.ecram.usersmicroecram.posts.repositories;
 
 import com.ecram.usersmicroecram.posts.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IPostRepository extends JpaRepository<Post, Long>, ICustomPostRepository {
+    @Query(" SELECT p FROM Post p                        " +
+           " join fetch p.blockPostList bpl              " +
+           " join fetch bpl.dataBlockPost dbp            " +
+           " join fetch dbp.fileBlockPostRelated fbpr    " +
+           " WHERE p.id = :postId AND p.status='enable'  ")
+    Post findPostData(@Param("postId") Long postId);
 
 }
--- src/main/java/com/ecram/usersmicroecram/posts/services/IPostOperationsService.java
+++ src/main/java/com/ecram/usersmicroecram/posts/services/IPostOperationsService.java
@@ -2,7 +2,6 @@ package com.ecram.usersmicroecram.posts.services;
 
 import com.ecram.usersmicroecram.posts.dtos.request.PostToCreateDto;
 import com.ecram.usersmicroecram.posts.dtos.response.PostCreatedDto;
-import com.ecram.usersmicroecram.posts.models.Post;
 
 public interface IPostOperationsService {
     PostCreatedDto createPost(Long userId, PostToCreateDto postToCreateDto);
